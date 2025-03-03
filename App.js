6410c48056d3c13e23defc70fab9bb229892fd45
Kavinbalasubramani <142481033+Kavinbalasubramani@users.noreply.github.com>
2025-03-03 18:13:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import StatsComponent from './Startscomponent';
+
+function App() {
+  return (
+    <div className="App">
+     <StatsComponent/>
+    </div>
+  );
+}
+
+export default App;
